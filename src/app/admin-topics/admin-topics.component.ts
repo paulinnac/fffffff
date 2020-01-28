694954c894768cf8155a396101a0a7455be2ccc4
rgamkiii <roberto.garciadealba@alumnos.udg.mx>
2020-01-28 12:30:15 -0600

    Commented the code

--- src/app/admin-topics/admin-topics.component.ts
+++ src/app/admin-topics/admin-topics.component.ts
@@ -5,10 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './admin-topics.component.html',
   styleUrls: ['./admin-topics.component.css']
 })
+/**
+ * Class to manage "Topics Due Date" table
+ */
 export class AdminTopicsComponent implements OnInit {
 
   //topics: Topic[];
-  topics: Topic[] = [
+  topics: any[] = [
     {description: 'link', dueDate: '01/01/2020', link: 'hppt://link'},
     {description: 'link', dueDate: '01/01/2020', link: 'hppt://link'},
     {description: 'link', dueDate: '01/01/2020', link: 'hppt://link'},
@@ -23,24 +26,32 @@ export class AdminTopicsComponent implements OnInit {
     {description: 'link', dueDate: '01/01/2020', link: 'hppt://link'},
   ];
   
-
   constructor() { console.log("AdminTopicsComponent constructor"); }
 
-  ngOnInit() { console.log(this.topics); console.log(this.getCurrentDate()); }
+  ngOnInit() { console.log(this.topics); console.log(this.getCurrentDate().toDateString().valueOf()); }
 
-  updateTopics(topics: Topic[]) {
+  /**
+   * // TODO: update values in this.topics array
+   * Gets the values in Topics Due Date table and assigns them to this.topics array
+   * @param topics 
+   */
+  updateTopics(topics: any[]) {
     console.log(topics);
-    topics.forEach(topic => {
-      //if (document.getElementById("topic").value 
-      topics.push(topic);
-    });
     this.showValues();
   }
 
+  /**
+   * Displays current values of Topic objects contained in this.topics array on the console
+   */
   showValues() {
     console.log(this.topics);
   }
 
+  /**
+   * // TODO: validate if topics' dates are close to due
+   * Highlight topics in table in regards of their dueDate proximity to the current date
+   * @param currentDate 
+   */
   validateDueDate(currentDate: Date) {
     this.topics.forEach(topic => {
       if(topic.dueDate.valueOf() > currentDate.toDateString().valueOf()) {
@@ -49,13 +60,20 @@ export class AdminTopicsComponent implements OnInit {
     });
   }
 
+  /**
+   * Returns a Date object containing the current date
+   * @returns current date
+   */
   getCurrentDate() {
-    let today = new Date();
-    console.log("fecha actual" + today);
-    return today;
+    let currentDate = new Date();
+    //console.log("fecha actual" + today);
+    return currentDate;
   }
 
 }
+/**
+ * Class that holds the data of a certain topic present in the table
+ */
 class Topic{
   constructor(public description: string, public dueDate: string, public link: string) { }
 }
