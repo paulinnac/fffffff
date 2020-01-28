743232f6edf352e68cb905bfb911bcaa37ad4831
rgamkiii <roberto.garciadealba@alumnos.udg.mx>
2020-01-28 12:54:30 -0600

    Added path to routing module for testing purpouses

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdminTopicsComponent } from './admin-topics/admin-topics.component';
+//import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  //{path: 'admin', component: AdminDashboardComponent}
+  {path: 'admin-topics', component: AdminTopicsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
